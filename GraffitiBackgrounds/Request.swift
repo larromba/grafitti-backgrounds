ad96a620e94c65da082c1b547d7e7c8f6c81a6ca
Lee Arromba <larromba@gmail.com>
2018-11-24 20:32:29 +0100

    fix: request

--- GraffitiBackgrounds/Request.swift
+++ GraffitiBackgrounds/Request.swift
@@ -5,19 +5,30 @@ protocol Request {
     var httpVerb: HTTPVerb { get }
     var body: Data? { get }
     var headers: [String: String]? { get }
+    var parameters: [String: String]? { get }
 }
 
 extension Request {
     var body: Data? {
         return nil
     }
-
     var headers: [String: String]? {
         return nil
     }
-
+    var parameters: [String: String]? {
+        return nil
+    }
     var urlRequest: URLRequest {
-        var request = URLRequest(url: url)
+        var components: URLComponents?
+        let finalURL: URL
+        if let parameters = parameters {
+            components = URLComponents(url: url, resolvingAgainstBaseURL: false)
+            components?.queryItems = parameters.map { URLQueryItem(name: $0.key, value: $0.value) }
+            finalURL = components?.url ?? url
+        } else {
+            finalURL = url
+        }
+        var request = URLRequest(url: finalURL)
         request.httpMethod = httpVerb.rawValue
         request.httpBody = body
         request.allHTTPHeaderFields = headers
